787390a22dbbfff3191aff19f0e22da0ec877523
eharrison <evandro@itsdayoff.com>
2020-05-21 13:34:47 +0800

    Initial Commit

---
+++ AVPlayerFade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVPlayerFade
+//
+//  Created by Evandro Harrison Hoffmann on 21/05/2020.
+//  Copyright © 2020 It's Day Off. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

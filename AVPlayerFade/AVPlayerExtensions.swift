a86cba0862ab94fe861ecbde0fcc27bf8d9f57c8
Evandro <evandro@itsdayoff.com>
2020-05-21 18:51:00 +0800

    [CHORE] Added readme file

---
+++ AVPlayerFade/AVPlayerExtensions.swift
@@ -0,0 +1,66 @@
+//
+//  AVPlayerExtensions.swift
+//  AVPlayerFade
+//
+//  Created by Evandro Harrison Hoffmann on 21/05/2020.
+//  Copyright © 2020 It's Day Off. All rights reserved.
+//
+
+import AVFoundation
+
+extension AVPlayer {
+    
+    /// Fades player volume FROM any volume TO any volume
+    /// - Parameters:
+    ///   - from: initial volume
+    ///   - to: target volume
+    ///   - duration: duration in seconds for the fade
+    ///   - completion: callback indicating completion
+    /// - Returns: Timer?
+    func fadeVolume(from: Float, to: Float, duration: Float, completion: (() -> Void)? = nil) -> Timer? {
+        
+        // 1. Set Initial volume
+        volume = from
+        
+        // 2. There's no point in continuing if target volume is the same as initial
+        guard from != to else { return nil }
+        
+        // 3. We define the time interval the interaction will loop into (fraction of a second)
+        let interval: Float = 0.1
+        // 4. Set the range the volume will move
+        let range = to-from
+        // 5. Based on the range, the interval and duration, we calculate how big is the step we need to take in order to reach the target in the given duration
+        let step = (range*interval)/duration
+        
+        // internal function whether the target has been reached or not
+        func reachedTarget() -> Bool {
+            // volume passed max/min
+            guard volume >= 0, volume <= 1 else {
+                volume = to
+                return true
+            }
+            
+            // checks whether the volume is going forward or backward and compare current volume to target
+            if to > from {
+                return volume >= to
+            }
+            return volume <= to
+        }
+        
+        // 6. We create a timer that will repeat itself with the given interval
+        return Timer.scheduledTimer(withTimeInterval: Double(interval), repeats: true, block: { [weak self] (timer) in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                // 7. Check if we reached the target, otherwise we add the volume
+                if !reachedTarget() {
+                    // note that if the step is negative, meaning that the to value is lower than the from value, the volume will be decreased instead
+                    self.volume += step
+                } else {
+                    timer.invalidate()
+                    completion?()
+                }
+            }
+        })
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# AVPlayer Fade
+
+## About
+
+The app demonstrates how to fade volume using AVPlayer.
+
+## Requirements
+
+- iOS 12+
+- Swift 5.0
+
+## Usage
+
+1. Copy **AVPlayerExtensions.swift** to your project
+2. Setup your AVPlayer and call the **fadeVolume** function
+
+For more detailed explanation, check article:
+https://medium.com/@evandro.hoffmann/fading-volume-with-avplayer-in-swift-74cbcc6172c6
+
+Happy coding! 😀

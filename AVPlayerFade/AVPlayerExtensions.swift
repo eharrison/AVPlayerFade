60d2d2ab8bc127fc7fcf72cd0dd523f15a51cdd3
Tushar Khandaker <56843185+khandakerTR@users.noreply.github.com>
2023-09-21 00:34:10 +0600

    added fadeOut

--- AVPlayerFade/AVPlayerExtensions.swift
+++ AVPlayerFade/AVPlayerExtensions.swift
@@ -17,7 +17,7 @@ extension AVPlayer {
     ///   - duration: duration in seconds for the fade
     ///   - completion: callback indicating completion
     /// - Returns: Timer?
-    func fadeVolume(from: Float, to: Float, duration: Float, completion: (() -> Void)? = nil) -> Timer? {
+    func fadeInVolume(from: Float, to: Float, duration: Float, completion: (() -> Void)? = nil) -> Timer? {
         
         // 1. Set Initial volume
         volume = from
@@ -56,6 +56,50 @@ extension AVPlayer {
                 if !reachedTarget() {
                     // note that if the step is negative, meaning that the to value is lower than the from value, the volume will be decreased instead
                     self.volume += step
+                } else {
+                    timer.invalidate()
+                    completion?()
+                }
+            }
+        })
+    }
+    
+    func fadeOutVolume(from: Float, to: Float, duration: Double, completion: (() -> Void)? = nil) -> Timer? {
+        
+        volume = from
+        guard from != to else { return nil }
+        
+        let interval: Float = 0.1
+        let range = from - to
+        let step = (range*interval) / Float(duration)
+        
+        func reachedTarget() -> Bool {
+            
+            guard volume >= 0, volume <= 1 else {
+                volume = from
+                return true
+            }
+            
+            if from > to {
+                return volume <= to
+            }
+            return volume >= to
+        }
+        
+        return Timer.scheduledTimer(withTimeInterval: Double(interval), repeats: true, block: { [weak self] (timer) in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                
+                if !reachedTarget() {
+                    if let currentItem = self.currentItem {
+                        let totalDuration = CMTimeGetSeconds(currentItem.duration)
+                        let currentTime = CMTimeGetSeconds(currentItem.currentTime())
+                        let fadeOutTime = totalDuration - duration
+                        if currentTime >= fadeOutTime {
+                            self.volume -= step
+                        }
+                    }
                 } else {
                     timer.invalidate()
                     completion?()
